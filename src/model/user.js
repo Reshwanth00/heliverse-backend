bc0c0916d5b9c96970b31c4d598109b4202ddc7f
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2024-08-15 10:29:24 +0530

    Update user.js

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    id:{
+        type:Number,
+        required:true
+    },
     name:{
         type:String,
         require: true
