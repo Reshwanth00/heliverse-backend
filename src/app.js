ba5abac37f0040630424d65ff384a977d62d5466
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2024-08-15 08:39:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 // app.use(cors({
 //   origin: 'http://localhost:3000'
 // }));
+app.use(cors());
 app.use('/api/principal', principalRoutes);
 app.use('/login',loginRoutes);
 
